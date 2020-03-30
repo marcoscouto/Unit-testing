6ef9092f9787da5e4f7ba941a39b828335cbce6e
Marcos Couto <marcos_otuoc@hotmail.com>
2020-03-30 19:51:00 -0300

    First test

--- src/main/java/com/marcoscouto/services/RentalService.java
+++ src/main/java/com/marcoscouto/services/RentalService.java
@@ -3,12 +3,14 @@ package com.marcoscouto.services;
 import com.marcoscouto.entities.Movie;
 import com.marcoscouto.entities.Rental;
 import com.marcoscouto.entities.User;
+import com.marcoscouto.utils.DateUtils;
 
 import java.util.Date;
 
 import static com.marcoscouto.utils.DateUtils.addDays;
 
 public class RentalService {
+
     public Rental rentMovie(User user, Movie movie) {
         Rental rental = new Rental();
         rental.setMovie(movie);
@@ -17,9 +19,9 @@ public class RentalService {
         rental.setPrice(movie.getPrice());
 
         //Entrega no dia seguinte
-        Date dataEntrega = new Date();
-        dataEntrega = addDays(dataEntrega, 1);
-        rental.setFinalDate(dataEntrega);
+        Date returnDate = new Date();
+        returnDate = addDays(returnDate, 1);
+        rental.setFinalDate(returnDate);
 
         //Salvando a locacao...
         //TODO adicionar método para salvar
@@ -28,6 +30,28 @@ public class RentalService {
     }
 
     public static void main(String[] args) {
+
+        //Cenário
+        RentalService rs = new RentalService();
+
+        User u = new User();
+        u.setName("Marcos");
+
+        Movie m = new Movie();
+        m.setName("A volta dos que não foram");
+        m.setStock(2);
+        m.setPrice(14.90);
+
+        //Ação
+
+        Rental rental = rs.rentMovie(u, m);
+
+        //Verificação
+
+        System.out.println(rental.getPrice() == 14.9);
+        System.out.println(DateUtils.isSameDate(rental.getInitialDate(), new Date()));
+        System.out.println(DateUtils.isSameDate(rental.getFinalDate(), DateUtils.obtaingDateWithDaysDifference(1)));
+
 
     }
 
