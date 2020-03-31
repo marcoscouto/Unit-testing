fe226fae99e44ecf58c69d0810985ee65dadf25a
Marcos Couto <marcos_otuoc@hotmail.com>
2020-03-31 20:07:28 -0300

    Error Collector

--- src/test/java/com/marcoscouto/services/RentalServiceTest.java
+++ src/test/java/com/marcoscouto/services/RentalServiceTest.java
@@ -4,14 +4,18 @@ import com.marcoscouto.entities.Movie;
 import com.marcoscouto.entities.Rental;
 import com.marcoscouto.entities.User;
 import com.marcoscouto.utils.DateUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import java.util.Date;
 
 public class RentalServiceTest {
 
-
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
 
     @Test
     public void test() {
@@ -33,9 +37,9 @@ public class RentalServiceTest {
 
         //Verificação
 
-        Assert.assertTrue(rental.getPrice() == 14.9);
-        Assert.assertTrue(DateUtils.isSameDate(rental.getInitialDate(), new Date()));
-        Assert.assertTrue(DateUtils.isSameDate(rental.getFinalDate(), DateUtils.obtaingDateWithDaysDifference(1)));
+        errorCollector.checkThat( rental.getPrice().doubleValue(), CoreMatchers.is(14.9));
+        errorCollector.checkThat(DateUtils.isSameDate(rental.getInitialDate(), new Date()), CoreMatchers.is(true));
+        errorCollector.checkThat(DateUtils.isSameDate(rental.getFinalDate(), DateUtils.obtaingDateWithDaysDifference(1)), CoreMatchers.is(true));
 
     }
 }
