ea7b0ff28bed097ece64ff330c5a78481319f6f3
Marcos Couto <marcos_otuoc@hotmail.com>
2020-03-31 17:43:57 -0300

    Organize test classes

--- src/main/java/com/marcoscouto/services/RentalService.java
+++ src/main/java/com/marcoscouto/services/RentalService.java
@@ -13,6 +13,8 @@ import static com.marcoscouto.utils.DateUtils.addDays;
 
 public class RentalService {
 
+
+
     public Rental rentMovie(User user, Movie movie) {
         Rental rental = new Rental();
         rental.setMovie(movie);
@@ -30,32 +32,5 @@ public class RentalService {
 
         return rental;
     }
-
-    @Test
-    public void test() {
-
-        //Cenário
-        RentalService rs = new RentalService();
-
-        User u = new User();
-        u.setName("Marcos");
-
-        Movie m = new Movie();
-        m.setName("A volta dos que não foram");
-        m.setStock(2);
-        m.setPrice(14.90);
-
-        //Ação
-
-        Rental rental = rs.rentMovie(u, m);
-
-        //Verificação
-
-        Assert.assertTrue(rental.getPrice() == 14.9);
-        Assert.assertTrue(DateUtils.isSameDate(rental.getInitialDate(), new Date()));
-        Assert.assertTrue(DateUtils.isSameDate(rental.getFinalDate(), DateUtils.obtaingDateWithDaysDifference(1)));
-
-    }
-
 
 }
